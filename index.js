e5654968b5c34ac01c41e5d795c2b6781365b688
Nadia Tsygankova <nadiapritchina@gmail.com>
2023-06-10 12:42:39 -0400

    add input

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ function renderEmotionsRadios(cats){
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+       // radioItems += `<p>${emotion}</p>`
+        radioItems +=
+        `<div class = "radio">
+        <input
+        type="radio"
+				id="${emotion}"
+				value="${emotion}"
+				name="choice-radios"/> <label for=${emotion}>${emotion}</label>
+        </div>`
     }
     emotionRadios.innerHTML = radioItems
 }
